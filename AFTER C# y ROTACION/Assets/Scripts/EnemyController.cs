de56e0c631ac65f9a064f30c5d929e5c4bc53b8a
Gaston Caminiti <gastoncaminiti@gmail.com>
2021-10-26 16:31:43 -0300

    Commit 2 [PRACTICA EN VIVO]
    Script PlayerController
    Script EnemyController
    Look at Lerp
    Move Around

--- AFTER C# y ROTACION/Assets/Scripts/EnemyController.cs
+++ AFTER C# y ROTACION/Assets/Scripts/EnemyController.cs
@@ -4,15 +4,80 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+
+    //ENUMERACION PARA DEFINIR TIPO DE MOVIMIENTO
+    enum TypesMovements { Linear , Around , Follow  };
+    //VARIABLES PRIVADAS CON EDICION DESDE EL EDITOR DE UNITY
+    [SerializeField] private TypesMovements typeMovement;
+    [SerializeField] private float speedEnemy = 5.0f;
+    [SerializeField] private GameObject aroundObject; //PIVOT
+    //VARIABLES PRIVADAS SIN EDICION DESDE EL EDITOR DE UNITY
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //MoveEnemy(Vector3.forward);
+        //LookAtLerp(player);
+        //MoveTowards();
+        //LookAtLerp(aroundObject);
+        // MoveAround(aroundObject);
+        switch (typeMovement)
+        {
+            case TypesMovements.Linear:
+                MoveEnemy(Vector3.forward);
+                break;
+            case TypesMovements.Around:
+                MoveAround(aroundObject);
+                break;
+            case TypesMovements.Follow:
+                LookAtLerp(player);
+                MoveTowards();
+                break;
+        }
+
+
+    }
+    //METODO PARA MOVER EL ENEMIGO EN UNA LINEA
+    private void MoveEnemy(Vector3 direction)
+    {
+        transform.Translate(speedEnemy * Time.deltaTime * direction);
+    }
+    //METODO PARA SEGUIR AL JUGADOR
+    private void MoveTowards()
+    {
+
+        //Vector3 direction = (player.transform.position - transform.position).normalized;
+        Vector3 direction = (player.transform.position - transform.position);
+        //Debug.Log(direction.magnitude);
+        if(direction.magnitude > 10)
+        {
+            transform.position += speedEnemy * direction.normalized * Time.deltaTime;
+        }
         
+    }
+    //METODO PARA MIRAR OBJETO
+    private void LookAt(GameObject lookObject)
+    {
+        Quaternion newRotation = Quaternion.LookRotation(lookObject.transform.position - transform.position);
+        transform.rotation = newRotation;
+    }
+
+    //METODO PARA MIRAR OBJETO CON LERP
+    private void LookAtLerp(GameObject lookObject)
+    {
+        Quaternion newRotation = Quaternion.LookRotation(lookObject.transform.position - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, 1f * Time.deltaTime);
+    }
+
+    //METODO MOVE AROUND
+    private void MoveAround(GameObject AroundObject)
+    {
+        transform.RotateAround(AroundObject.transform.position, Vector3.up, speedEnemy * Time.deltaTime);
     }
 }
--- AFTER C# y ROTACION/Assets/Scripts/PlayerController.cs
+++ AFTER C# y ROTACION/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    //VARIABLES PRIVADAS CON EDICION DESDE EL EDITOR DE UNITY
+    [SerializeField] private float speedPlayer = 5f;
+    [SerializeField] private float rotationSpeed = 10f;
+    //VARIABLES PRIVADAS SIN EDICION DESDE EL EDITOR DE UNITY
+    private float mouseAxisX;
     void Start()
     {
         
@@ -13,6 +18,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //LLAMADA AL METODO PARA MOVER EL JUGADOR EN CADA FRAME;
+        RotatePlayer2();
+        MovePlayer();
+    }
+
+    //METODO PARA MOVER EL PLAYER EN FUNCION DE LOS AXIS HORIZONTAL Y VERTICAL
+    private void MovePlayer()
+    {
+        //float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+        transform.Translate(speedPlayer * new Vector3(0, 0, vAxis) * Time.deltaTime);
+    }
+    //METODO PARA ROTAR EL PLAYER EN FUNCION DE AXIS MOUSE X
+    private void RotatePlayer()
+    {
+        mouseAxisX += Input.GetAxis("Mouse X");
+        Quaternion newRotation = Quaternion.Euler(0, mouseAxisX, 0);
+        transform.rotation = newRotation;
+    }
+
+    //METODO PARA ROTAR EL PLAYER EN FUNCION DE AXIS VERTICAL
+    private void RotatePlayer2()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, hAxis * rotationSpeed * Time.deltaTime);
     }
 }
